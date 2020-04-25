681f8047749831e919df0b64e0f691f6a71a1a5a
Маша <masha.kustova-guselnikova@yandex.ru>
2020-04-25 07:59:48 +0500

    Добавляет изображения в формате webp

---
+++ source/js/index.js
@@ -0,0 +1,30 @@
+var buttonMenu = document.querySelector(".main-nav__toggle");
+var navigation = document.querySelector(".main-nav");
+var buttonOrder = document.querySelector(".product-week__button");
+var buttonAdd = document.querySelector(".catalog-popup__button");
+var popup = document.querySelector(".catalog-popup");
+var body = document.querySelector(".body__wrapper");
+
+navigation.classList.remove("main-nav--nojs");
+
+buttonMenu.addEventListener("click", function () {
+  if (navigation.classList.contains("main-nav--closed")) {
+    navigation.classList.remove("main-nav--closed");
+    navigation.classList.add("main-nav--opened");
+  } else {
+    navigation.classList.add("main-nav--closed");
+    navigation.classList.remove("main-nav--opened");
+  }
+});
+
+buttonOrder.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("catalog-popup--opened");
+  body.classList.add("body__wrapper--opacity");
+});
+
+buttonAdd.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("catalog-popup--opened");
+  body.classList.remove("body__wrapper--opacity");
+});
